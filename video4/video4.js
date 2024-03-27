0ed22555c2147a7d74a8753973baba69c4a2392a
EmmTyyy <thang.lnm23120@sinhvien.hoasen.edu.vn>
2024-03-27 20:31:15 +0700

    chapter4 video 4

---
+++ video4/video4.js
@@ -0,0 +1,13 @@
+let a = 5;
+let c = 'Xin Chào EmmTyyy'
+let b =4;
+console.log(c)
+console.log('c' + a + b ); // = 9 +'a' =9a
+console.log(a + b +'c' ); // = a5 +4 =a54
+// Use  `` the best 
+let text =  `We are the so-called "Viking" from the North`
+let text2 =  `We are the so-called 
+"Viking" from the North`
+
+console.log(text)
+console.log(text2)
