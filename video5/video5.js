f391544346a86521991c0d699bd48ec5887fd37e
EmmTyyy <thang.lnm23120@sinhvien.hoasen.edu.vn>
2024-04-01 13:51:05 +0700

    vido8

--- video5/video5.js
+++ video5/video5.js
@@ -1,4 +1,4 @@
 let text1 = 'Lam Nguyen Minh Thang';
-console.log(text1.toUpperCase());
+    console.log(text1.toUpperCase());
 console.log(text1.toLowerCase());
 console.log(text1.trim().length);
---
+++ video8/video8.js
@@ -0,0 +1,25 @@
+console.log("Console console");
+// key : value
+//syntax( sai cu phap)
+//convention (qui tắc code)
+let obj ={
+    name :  'EmmTyy',
+    address : 'HCM',
+    email : '',
+    gender:'',
+    // a :function () {
+    //     console.log('hi')
+    //     return ''
+    // } it khi su dung         
+};
+JSON
+"{name:'EmmTyy',address:'HCM'}"
+let b = 'Eric';
+// obj[b] = 'hary' tao key b moi
+obj.name ='hcm'
+let c = '';
+console.log("What is ur name ? ",`My name's `,obj['name'],);
+console.log("type of a",typeof a,"type of b",typeof b,);
+console.log("What is ur name ? ",`My name's `,obj.name,);
+console.log("Where r you from ? ",`I'm from `,obj.address,);
+// console.log("call function",obj.a())
