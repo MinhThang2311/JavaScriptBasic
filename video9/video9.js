efe1836a0300237c6971ce7c6883535057166847
EmmTyyy <thang.lnm23120@sinhvien.hoasen.edu.vn>
2024-04-01 23:29:44 +0700

    video9

---
+++ video9/video9.js
@@ -0,0 +1,23 @@
+console.log(`hello world`)
+let a = {
+    name : 'Thang',
+    age :   24
+}; //key : value
+let b = ['MU','Chelsea','Liverpool'];
+
+let family = [
+    { name : `EmmTyyy`,age : 24},
+    { name : `Meow`,age :29},
+]
+let cars = ["Saab", "Volvo", "BMW"];
+let car = cars[1];
+cars[2] = `EmmTyyy`
+console.log(`car?`,cars)
+console.log(`car to string?`,cars.toString())
+console.log(`my farmily`,family);
+console.log(`type of b `,typeof b,b);
+console.log(`first item `,b[0],`second item`,b[1]);
+console.log(`check data object`,a)
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(`length of fruits`,fruits.length)
